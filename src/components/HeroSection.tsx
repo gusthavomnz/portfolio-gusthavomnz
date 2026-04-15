4397e5242a3ecf1bab5861d2d7dc3c3ff18900ec
Gusthavo <gusthavogp123@gmail.com>
2026-04-15 16:10:00 -0300

    feat: fragmentação da HomePage em HeroSection para criação de componentes.

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,12 @@
+function HeroSection() {
+    return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-900">
+      <h1 className="text-4xl font-bold text-white">
+        Gusthavo Menezes </h1>
+    <h1 className = "text-2xl font-light text-gray-300">
+      github.com/gusthavomnz
+    </h1>
+    </div>
+)}
+
+export default HeroSection;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,10 @@
+import HeroSection from "../components/HeroSection";
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>Portfolio online em construção. </h1>
+    <div>
+      <HeroSection />
     </div>
+    
   );
 }
