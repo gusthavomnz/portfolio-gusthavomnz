52aaa5c5859a98ff1845dee0bfd0a132bfb77a71
Gusthavo <gusthavogp123@gmail.com>
2026-04-15 17:04:22 -0300

    feat: criação da estrutura do component ContatoSection

---
+++ src/components/ContatoSection.tsx
@@ -0,0 +1,10 @@
+function ContatoSection() {
+    return (
+        <div className=" bg-sky-950">
+            <h1 className="text-2xl flex flex-col font-bold text-white items-center px-4 py-4 ">
+                Contato
+            </h1>
+        </div>
+    )
+}
+export default ContatoSection;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "../components/HeroSection";
 import FormacaoSection from "../components/FormaçãoSection";
 import ProjetosSection from "../components/ProjetosSection";
+import ContatoSection from "../components/ContatoSection";
 
 export default function HomePage() {
   return (
@@ -8,6 +9,7 @@ export default function HomePage() {
       <HeroSection />
       <FormacaoSection />
       <ProjetosSection />
+      <ContatoSection />
     </div>
     
   );
