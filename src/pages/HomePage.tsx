705d3e04776ef099fb4d1080649c00922942ed51
Gusthavo <gusthavogp123@gmail.com>
2026-04-15 15:41:47 -0300

    :tada: Commit inicial. Configuração inicial do projeto

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+
+export default function HomePage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Portfolio online em construção. </h1>
+    </div>
+  );
+}
